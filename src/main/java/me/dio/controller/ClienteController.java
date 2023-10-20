2b0d29d208b4f2fed510aa9828eb188ade44f3ab
Gabriel Henrique <gabrielh.jrf@gmail.com>
2023-10-20 15:31:11 -0300

    feat: add controller

---
+++ src/main/java/me/dio/controller/ClienteController.java
@@ -0,0 +1,10 @@
+package me.dio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("clientes")
+public class ClienteController {
+    
+}
